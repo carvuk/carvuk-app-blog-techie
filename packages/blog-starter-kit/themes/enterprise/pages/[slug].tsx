c1e235d273d0f216eb5822d013cdf365d09bb27a
Nicolasvegam <ndvega@uc.cl>
2024-01-14 11:30:42 -0300

    delete: canonical head 🔥

--- packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
@@ -36,15 +36,15 @@ const PostComments = dynamic(() =>
 
 type Props =
 	| {
-			post: PostFullFragment;
-			page: null;
-			publication: PublicationFragment;
-	  }
+		post: PostFullFragment;
+		page: null;
+		publication: PublicationFragment;
+	}
 	| {
-			post: null;
-			page: StaticPageFragment;
-			publication: PublicationFragment;
-	  };
+		post: null;
+		page: StaticPageFragment;
+		publication: PublicationFragment;
+	};
 
 const Post = (publication: PublicationFragment, post: PostFullFragment) => {
 	const highlightJsMonokaiTheme =
@@ -65,7 +65,6 @@ const Post = (publication: PublicationFragment, post: PostFullFragment) => {
 		<>
 			<Head>
 				<title>{post.seo?.title || post.title}</title>
-				<link rel="canonical" href={post.url} />
 				<meta name="description" content={post.seo?.description || post.subtitle || post.brief} />
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta property="twitter:title" content={post.seo?.title || post.title} />
